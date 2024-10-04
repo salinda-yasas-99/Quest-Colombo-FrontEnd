fd4a41f23de4608d6cc3d054b2d0e82ec57cd51f
Dominic Lakshan <dominiclakshank@gmail.com>
2024-10-05 00:59:59 +0530

    removing inline css

--- src/components/home/FeaturesSection.jsx
+++ src/components/home/FeaturesSection.jsx
@@ -11,89 +11,51 @@ const { Title } = Typography;
 
 const FeaturesSection = () => {
   return (
-    <div
-      style={{
-        padding: "20px 20px 50px 20px",
-        backgroundColor: "white",
-        borderRadius: "12px",
-      }}
-    >
-      <Title style={{ textAlign: "center", color: "#00b96b" }}>
-        Our Features
-      </Title>
+    <div className="home-feature-section">
+      <Title className="home-feature-section-title ">Our Features</Title>
       <Row gutter={[16, 16]} justify="center">
         <Col xs={24} sm={12} md={8}>
           <Card
+            className="home-feature-section-card"
             hoverable
             cover={
-              <CalendarOutlined
-                style={{
-                  fontSize: "64px",
-                  color: "#00b96b",
-                  marginTop: "24px",
-                }}
-              />
+              <CalendarOutlined className="home-feature-section-card-icon" />
             }
-            style={{
-              borderRadius: "12px",
-            }}
           >
             <Meta
+              className="home-feature-section-card-meta"
               title="Real-Time Bookin"
               description="Book and manage spaces easily with real-time updates."
-              style={{
-                textAlign: "center",
-              }}
             />
           </Card>
         </Col>
         <Col xs={24} sm={12} md={8}>
           <Card
+            className="home-feature-section-card"
             hoverable
             cover={
-              <VideoCameraOutlined
-                style={{
-                  fontSize: "64px",
-                  color: "#00b96b",
-                  marginTop: "24px",
-                }}
-              />
+              <VideoCameraOutlined className="home-feature-section-card-icon" />
             }
-            style={{
-              borderRadius: "12px",
-            }}
           >
             <Meta
+              className="home-feature-section-card-meta"
               title="360-Degree View"
               description="Visualize the space before booking with a 360-degree view."
-              style={{
-                textAlign: "center",
-              }}
             />
           </Card>
         </Col>
         <Col xs={24} sm={12} md={8}>
           <Card
+            className="home-feature-section-card"
             hoverable
             cover={
-              <DashboardOutlined
-                style={{
-                  fontSize: "64px",
-                  color: "#00b96b",
-                  marginTop: "24px",
-                }}
-              />
+              <DashboardOutlined className="home-feature-section-card-icon" />
             }
-            style={{
-              borderRadius: "12px",
-            }}
           >
             <Meta
+              className="home-feature-section-card-meta"
               title="Admin Dashboard"
               description="Manage the entire system with a powerful admin dashboard."
-              style={{
-                textAlign: "center",
-              }}
             />
           </Card>
         </Col>
